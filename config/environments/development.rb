f97fc324fb702c8328b7638b9e73e64abb6957fa
Jamison Rubino <jamison.rubino@gmail.com>
2018-10-04 16:43:05 -0700

    Updated mailer to use sendgrid.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,18 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :domain               => 'mail.google.com',
+   :port                 => 587,
+   :user_name            => ENV['gmail_username'],
+   :password             => ENV['gmail_password'],
+   :authentication       => "plain",
+   :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -39,16 +50,5 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
-  # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :domain               => 'mail.google.com',
-   :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
-   :authentication       => "plain",
-   :enable_starttls_auto => true
-  }
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,14 +69,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'https://links-handler.herokuapp.com' }
-  
+
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :domain               => 'mail.google.com',
+   :address              => "smtp.sendgrid.net",
+   :domain               => 'links-handler.herokuapp.com',
    :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
+   :user_name            => ENV['sendgrid_username'],
+   :password             => ENV['sendgrid_password'],
    :authentication       => "plain",
    :enable_starttls_auto => true
   }
