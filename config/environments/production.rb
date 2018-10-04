e8f9decaaa7ee2fcfe6873361a9cdfac3a218d82
Jamison Rubino <jamison.rubino@gmail.com>
2018-10-04 16:06:27 -0700

    config>environments>production.rb default_url_options updated for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'https://links-handler.herokuapp.com' }
   
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
